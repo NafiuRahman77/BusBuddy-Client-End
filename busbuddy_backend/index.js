772ed8c63ea46570305b9edf2514c35cc4ba0f2a
Wasif Jalal <jalalwasif@gmail.com>
2024-01-28 01:29:00 +0600

    local to server push

--- busbuddy_backend/index.js
+++ busbuddy_backend/index.js
@@ -988,6 +988,24 @@ app.post('/api/updateStaffLocation', (req,res) => {
 app.post('/api/updateTripT', (req,res) => {
     //send a dummy response
     console.log(pd.trip_t);
+    for (let i=0; i<trip_t.path.length; i++) {
+        console.log(`(${t.path[i].latitude}, ${t.path[i].longitude})`);
+    };
+    // dbclient.query(
+    //     `update trip set passenger_count=$1, is_live=false where id=$2 and (driver=$3 or helper=$3)`, 
+    //     [trip_t.passenger_count, trip_t.body.latitude, 'altaf']
+    // ).then(qres => {
+    //     console.log(qres);
+    //     if (qres.rowCount === 1) res.send({ 
+    //         success: true,
+    //     });
+    //     else if (qres.rowCount === 0) {
+    //         res.send({
+    //             success: false,
+    //         });
+    //     };
+    // }).catch(e => console.error(e.stack));
+    // tracking.runningTrips.delete(req.body.trip_id);
 });
 
 app.post('/api/staffScanTicket', (req,res) => {
