c7b4b0b144ca5fb75df4d716923c4f271d3e7f35
WJGalib <jalalwasif@gmail.com>
2024-02-06 01:01:57 +0600

    logging

--- busbuddy_backend/index.js
+++ busbuddy_backend/index.js
@@ -21,7 +21,7 @@ const { createHttpTerminator } = require('http-terminator');
 const bcrypt = require('bcryptjs');
 const bcryptSaltRounds = 12;
 const log = fs.createWriteStream("append.txt", {flags:'a'});
-const writeToLog = str => log.write(new Date() + "\t" + str);
+const writeToLog = str => log.write(new Date() + "\t" + str.toString() + "\n");
 
 dotenv.config();
 
