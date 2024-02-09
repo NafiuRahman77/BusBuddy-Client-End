a2705eceecb5b5664e1ede4a32fd43de731fa8ff
WJGalib <jalalwasif@gmail.com>
2024-02-09 23:12:03 +0600

    logger

--- busbuddy_backend/index.js
+++ busbuddy_backend/index.js
@@ -48,7 +48,7 @@ const errLogger = log4js.getLogger("err");
 const readline = require('readline');
 
 const reqLogger = (req, res, next) => {
-    
+    consoleLogger.info (`Request at ${req.originalUrl} from ${req.session? (req.session.userid? req.session.userid : "") : "" } (${req.ip})`);
     next();
 };
 
@@ -246,7 +246,6 @@ app.post('/api/login', (req, res) => {
 });
 
 app.post('/api/sessionCheck', (req, res) => {
-    consoleLogger.info (`Request at ${req.originalUrl} from ${req.session? (req.session.userid? req.session.userid : "") : "" } (${req.ip})`);
     if (req.session.userid) {
         req.session.fcm_id = req.body.fcm_id;
         res.send({
